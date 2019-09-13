9e87a18f07686010bd116dbd2473158e750449b7
Patrick Matthäi <pmd.matthaei@gmail.com>
2019-09-13 15:08:00 +0200

    Interactive plotting

--- plots/plotter.py
+++ plots/plotter.py
@@ -6,7 +6,7 @@ from wrappers.gym_wrapper import ThrowEnvWrapper
 import gym
 
 # TODO: Delete this file when its knowledge is no longer needed.
-STEPS = 200
+STEPS = 100
 EPISODES = 20
 PAUSE = 1e-6
 
@@ -15,10 +15,8 @@ def initialize():
     ## interactive plotting on (no need to close window for next iteration)
     plt.ion()
     plt.grid()
-    plt.ylim(0, 2)  # limit y axis
+    plt.ylim(20, 20)  # limit y axis
     plt.title("Please wait for the end of the first episode.")
-    plt.ylabel("Reward per step")
-    plt.xlabel("Play")
 
 
 def close():
@@ -27,8 +25,9 @@ def close():
     plt.show()
 
 
-def plot(reward_memory, method_name, episode, steps):
+def plot_avg_reward_per_step(reward_memory, method_name, episode, steps):
     ci = 0.95  # 95% confidence interval
+    # axis=0 results in the mean of reward at each step not in the episode
     reward_means = np.mean(reward_memory, axis=0)
     stds = np.std(reward_memory, axis=0)
 
@@ -49,10 +48,31 @@ def plot(reward_memory, method_name, episode, steps):
     x = np.arange(0, steps, 1)
     ax = plt.fill_between(x=x, y1=lower_bound, y2=upper_bound, color='blue', alpha=0.2, label="CI %.2f" % ci)
     plt.grid()
-    plt.ylim(0, 2)  # limit y axis
+    plt.ylim(-20, 20)  # limit y axis
     plt.title(method_name + ': Avg. Reward per step in experiment %d: %.4f' % (episode, sum(reward_means) / steps))
     plt.ylabel("Reward per step")
     plt.xlabel("Play")
+    plt.legend()
+    plt.draw()
+    plt.pause(PAUSE)
+
+
+def plot_reward_per_step(rewards, method_name, episode, steps):
+    print('Avg. Reward in experiment %d: %.4f' % (episode, np.mean(rewards)))
+
+    # clear plot frame
+    plt.clf()
+
+    # plot average reward
+    ax = plt.plot(rewards, color='blue', label="reward")
+
+    # plot upper/lower confidence bound
+    x = np.arange(0, steps, 1)
+    plt.grid()
+    plt.ylim(-20, 20)  # limit y axis
+    plt.title(method_name + ': Avg. Reward in experiment %d: %.4f' % (episode, np.mean(rewards)))
+    plt.ylabel("Reward")
+    plt.xlabel("Step")
     plt.legend()
     plt.draw()
     plt.pause(PAUSE)
@@ -66,19 +86,19 @@ initialize()
 for episode in range(EPISODES):
 
     obs = env.reset()
-    rewards = []
+    episode_rewards = np.zeros(STEPS)
     for step in range(STEPS):
         obs, reward, done, info = env.step(env.action_space.sample())
-        rewards.append(reward)
+        episode_rewards[step] = reward
         if done:
             break
         env.render()
         plt.pause(PAUSE)
 
-    reward_memory.append(rewards)
+    reward_memory.append(episode_rewards)
+    #plot_avg_reward_per_step(reward_memory, 'DMP', episode + 1, STEPS)
 
-    plot(reward_memory, 'DMP', episode + 1, STEPS)
-
+    plot_reward_per_step(episode_rewards, 'DMP', episode + 1, STEPS)
 
 env.close()
 close()
